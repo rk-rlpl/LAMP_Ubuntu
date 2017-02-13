73794707a0a935bdb1999a4623681aeb0ebb85fb
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-02-13 17:05:30 +0530

    add apache2 installation

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2017, Raj Kiran Gade, All Rights Reserved.
+
+# Install Apache2 from package
+package 'apache2' do
+  action [:install]
+end
+
+service 'apache2' do
+  # supports :status => true
+  action [:enable, :start]
+end
